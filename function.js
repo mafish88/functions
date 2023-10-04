adcd1cf2926553e439aa57ab70ec6ea761143531
Michael Fisher <michaelfisher@Michaels-MacBook-Air.local>
2023-10-04 16:26:07 -0400

    created some JS functions

---
+++ function.js
@@ -0,0 +1,16 @@
+function returnsSomething(){
+    console.log("hello")
+    return 5;
+}
+
+const value = returnsSomething()
+
+console.log(value)
+
+
+function celciustofahrenheit(celsius){
+    return(celsius * 9/5 + 32)
+
+}
+
+console.log(celciustofahrenheit(32) + '°F')
---
+++ index.js
@@ -0,0 +1,12 @@
+function hello() {
+    // const firstName = "todd"
+    console.log("hello" + firstName)
+}firstName = "todd"
+hello("todd") 
+
+
+
+function add(num1, num2){
+    console.log(num1 + num2)
+}
+add(20, 15)
